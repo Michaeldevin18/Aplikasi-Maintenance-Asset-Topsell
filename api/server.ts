fa358b9b5675c4457f96815b7a744903787b7e56
TopsellDev <dev@topsell.com>
2026-02-18 13:51:19 +0700

    Stabilize dev server restarts

--- api/server.ts
+++ api/server.ts
@@ -12,23 +12,37 @@ const server = app.listen(PORT, () => {
   console.log(`Server ready on port ${PORT}`);
 });
 
+server.on('error', (err: NodeJS.ErrnoException) => {
+  if (err.code === 'EADDRINUSE') {
+    console.error(`Port ${PORT} is already in use. Stop the other process or set PORT.`);
+    process.exit(1);
+  }
+
+  console.error('Server error:', err);
+  process.exit(1);
+});
+
+const shutdown = (signal: string, cb?: () => void) => {
+  console.log(`${signal} signal received`);
+  server.close(() => {
+    console.log('Server closed');
+    cb?.();
+  });
+};
+
 /**
  * close server
  */
 process.on('SIGTERM', () => {
-  console.log('SIGTERM signal received');
-  server.close(() => {
-    console.log('Server closed');
-    process.exit(0);
-  });
+  shutdown('SIGTERM', () => process.exit(0));
 });
 
 process.on('SIGINT', () => {
-  console.log('SIGINT signal received');
-  server.close(() => {
-    console.log('Server closed');
-    process.exit(0);
-  });
+  shutdown('SIGINT', () => process.exit(0));
+});
+
+process.once('SIGUSR2', () => {
+  shutdown('SIGUSR2', () => process.kill(process.pid, 'SIGUSR2'));
 });
 
 export default app;
